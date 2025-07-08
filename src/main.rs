29e970f55eaacaf92e486bb8128bd0b05fc7ef00
James Bartlett <jamesbartlett02@gmail.com>
2025-07-08 19:56:44 +1000

    commit minimal Rust Kernel

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
+
+
+#[unsafe(no_mangle)]  // Ensure the name of the function doesn't get mangled
+pub extern "C" fn _start() -> ! {
+    // This function is the entry point, named _start by default
+
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop{}
+}
+
+/// This function is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop{}
+}
+
